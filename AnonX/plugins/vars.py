320a2865e51733964eed3a974aa2fc6738a4d4ab
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-01-18 02:29:18 +0530

    Update vars.py

--- AnonX/plugins/vars.py
+++ AnonX/plugins/vars.py
@@ -1,22 +1,13 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import asyncio
 
 from pyrogram import filters
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database.memorydatabase import get_video_limit
-from YukkiMusic.utils.formatters import convert_bytes
+from AnonX import app
+from AnonX.misc import SUDOERS
+from AnonX.utils.database.memorydatabase import get_video_limit
+from AnonX.utils.formatters import convert_bytes
 
 VARS_COMMAND = get_command("VARS_COMMAND")
 
